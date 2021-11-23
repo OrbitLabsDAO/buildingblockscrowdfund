016ab2118084899a75d09737d6cce84b29312715
chris <cryptoskillz@chriss-MacBook-Air.local>
2021-11-23 17:58:09 +0700

    functions update

--- _data/api.js
+++ _data/api.js
@@ -1,10 +1,14 @@
 
-/*
-remove the comments when you have wired up the API
-require('dotenv').config();
 
-//mark down module
-const toMarkdown = require('@sanity/block-content-to-markdown')
+//remove the comments when you have wired up the API
+require('dotenv').config();
+//debug
+//console.log(process.env.SANITYPROJECTID)
+
+const blocksToHtml = require('@sanity/block-content-to-html')
+const h = blocksToHtml.h
+
+
 //sanitu client
 const sanityClient = require('@sanity/client')
 //configure sanity
@@ -12,14 +16,20 @@ const client = sanityClient({
   projectId: process.env.SANITYPROJECTID,
   dataset: process.env.SANITYDATASET,
   apiVersion: process.env.SANITYAPIVERSION, // use current UTC date - see "specifying API version"!
-  token: process.env.SANITYTOKEN', // or leave blank for unauthenticated usage
+  token: process.env.SANITYTOKEN, // or leave blank for unauthenticated usage
   useCdn: false, // `false` if you want to ensure fresh data
 })
 
-//seralizers
+
+
 const serializers = {
   types: {
-    code: props => '```' + props.node.language + '\n' + props.node.code + '\n```'
+    code: props => (
+      h('pre', {className: props.node.language},
+        h('code', props.node.code),
+        h('image: props =&gt; &lt;img ... /&gt;')
+      )
+    )
   }
 }
 
@@ -42,14 +52,28 @@ module.exports = async () => {
   let posts = []
   //call the get posts fuction
   if (posts.length === 0) posts =  await getPosts();
+
+  //for html
+  posts[0]._body= blocksToHtml({
+    blocks: posts[0].body,
+    serializers: serializers
+  })
+
+  //build an  image URL 
+  //note : we know there is a way to do this cleaner but I have hacked it for now
+  let _imageurl = `https://cdn.sanity.io/images/${process.env.SANITYPROJECTID}/production/`;
+  let _image = posts[0].mainImage.asset._ref;
+  _image = _image.replace("image-","")
+  _image = _image.replace("-png",".png")
+  posts[0]._mainImage = _imageurl+_image
   //debug
   //console.log("posts")
-  //console.log(toMarkdown(posts.body, {serializers}))
-  //console.dir(posts)
-  
+  //console.log(posts)
+  //console.dir(posts[0].body)
+  //console.log(posts[0])
+  //console.log(posts[0]._mainImage)
   return {
         postsArray: posts
   }
 
 }
-*/
